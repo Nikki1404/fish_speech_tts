06320346dfef4c3ed26117b1cf734be3f9db186e
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-08-04 15:58:04 +0530

    Update update.py

--- update.py
+++ update.py
@@ -6,9 +6,65 @@ DOCKER_BUILDKIT=1 docker build \
   --progress=plain \
   -t fish-s2-cpu:latest \
 
-docker run -d --cpus=12 --shm-size=8g -p 8000:8000 -e DEVICE=cpu -e OMP_NUM_THREADS=12 -e MKL_NUM_THREADS=12 -e OPENBLAS_NUM_THREADS=12 fish-s2-cpu:latest
-(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/fish_speech_tts# docker run -d --cpus=12 --shm-size=8g -p 8000:8000 -e DEVICE=cpu -e OMP_NUM_THREADS=12 -e MKL_NUM_THREADS=12 -e OPENBLAS_NUM_THREADS=12 fish-s2-cpu:latest
-docker: Error response from daemon: range of CPUs is from 0.01 to 8.00, as there are only 8 CPUs available
+docker run -d --shm-size=8g -p 8000:8000 -e DEVICE=cpu  fish-s2-cpu:latest
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/fish_speech_tts# docker logs a53f4b64b248
+[2026-08-04 10:25:41] Starting Fish Speech API Server...
+[2026-08-04 10:25:41] Device args: --device cpu
+[2026-08-04 10:25:41] Compile args: uvicorn app.main:app --host 0.0.0.0 --port 8000
+[2026-08-04 10:25:41] Server: 0.0.0.0:8080
+   Building fish-speech @ file:///app
+      Built fish-speech @ file:///app
+Uninstalled 1 package in 15ms
+Installed 1 package in 3ms
+/app/.venv/lib/python3.12/site-packages/audiotools/core/audio_signal.py:44: SyntaxWarning: invalid escape sequence '\_'
+  Type of window to use, by default ``sqrt\_hann``.
+/app/.venv/lib/python3.12/site-packages/audiotools/core/audio_signal.py:1014: SyntaxWarning: invalid escape sequence '\_'
+  using functools.lru\_cache.
+/app/.venv/lib/python3.12/site-packages/audiotools/core/audio_signal.py:1092: SyntaxWarning: invalid escape sequence '\_'
+  """Compute how the STFT should be padded, based on match\_stride.
+/app/.venv/lib/python3.12/site-packages/audiotools/core/audio_signal.py:1141: SyntaxWarning: invalid escape sequence '\_'
+  Type of window to use, by default ``sqrt\_hann``.
+/app/.venv/lib/python3.12/site-packages/audiotools/core/audio_signal.py:1222: SyntaxWarning: invalid escape sequence '\_'
+  """Computes inverse STFT and sets it to audio\_data.
+/app/.venv/lib/python3.12/site-packages/torch/amp/autocast_mode.py:266: UserWarning: User provided device_type of 'cuda', but CUDA is not available. Disabling
+  warnings.warn(
+usage: api_server.py [-h] [--mode {tts}]
+                     [--llama-checkpoint-path LLAMA_CHECKPOINT_PATH]
+                     [--decoder-checkpoint-path DECODER_CHECKPOINT_PATH]
+                     [--decoder-config-name DECODER_CONFIG_NAME]
+                     [--device DEVICE] [--half] [--compile]
+                     [--max-text-length MAX_TEXT_LENGTH] [--listen LISTEN]
+                     [--workers WORKERS] [--api-key API_KEY]
+api_server.py: error: unrecognized arguments: uvicorn app.main:app --host 0.0.0.0 --port 8000
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/fish_speech_tts# docker logs a53f4b64b248
+[2026-08-04 10:25:41] Starting Fish Speech API Server...
+[2026-08-04 10:25:41] Device args: --device cpu
+[2026-08-04 10:25:41] Compile args: uvicorn app.main:app --host 0.0.0.0 --port 8000
+[2026-08-04 10:25:41] Server: 0.0.0.0:8080
+   Building fish-speech @ file:///app
+      Built fish-speech @ file:///app
+Uninstalled 1 package in 15ms
+Installed 1 package in 3ms
+/app/.venv/lib/python3.12/site-packages/audiotools/core/audio_signal.py:44: SyntaxWarning: invalid escape sequence '\_'
+  Type of window to use, by default ``sqrt\_hann``.
+/app/.venv/lib/python3.12/site-packages/audiotools/core/audio_signal.py:1014: SyntaxWarning: invalid escape sequence '\_'
+  using functools.lru\_cache.
+/app/.venv/lib/python3.12/site-packages/audiotools/core/audio_signal.py:1092: SyntaxWarning: invalid escape sequence '\_'
+  """Compute how the STFT should be padded, based on match\_stride.
+/app/.venv/lib/python3.12/site-packages/audiotools/core/audio_signal.py:1141: SyntaxWarning: invalid escape sequence '\_'
+  Type of window to use, by default ``sqrt\_hann``.
+/app/.venv/lib/python3.12/site-packages/audiotools/core/audio_signal.py:1222: SyntaxWarning: invalid escape sequence '\_'
+  """Computes inverse STFT and sets it to audio\_data.
+/app/.venv/lib/python3.12/site-packages/torch/amp/autocast_mode.py:266: UserWarning: User provided device_type of 'cuda', but CUDA is not available. Disabling
+  warnings.warn(
+usage: api_server.py [-h] [--mode {tts}]
+                     [--llama-checkpoint-path LLAMA_CHECKPOINT_PATH]
+                     [--decoder-checkpoint-path DECODER_CHECKPOINT_PATH]
+                     [--decoder-config-name DECODER_CONFIG_NAME]
+                     [--device DEVICE] [--half] [--compile]
+                     [--max-text-length MAX_TEXT_LENGTH] [--listen LISTEN]
+                     [--workers WORKERS] [--api-key API_KEY]
+api_server.py: error: unrecognized arguments: uvicorn app.main:app --host 0.0.0.0 --port 8000
 
 
 Run 'docker run --help' for more information
