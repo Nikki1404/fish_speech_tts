6d63bc15463d8ef3783603c0a7b8ce894b9d2fc1
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-08-10 18:50:11 +0530

    Update update1.py

--- update1.py
+++ update1.py
@@ -24,3 +24,33 @@ docker run -d --gpus all --name qwen3-tts -p 8003:8003 qwen3-tts
 
 
 python client.py --server ws://<EC2-IP>:8001/ws/tts --text "I cannot believe we finally made it!" --language English --speaker Aiden --instruct "Speak happily and with excitement." --output excited.wav
+
+
+  PS C:\Users\re_nikitav\Documents\quen_3_tts> python client.py --server ws://localhost:8003/ws/tts --text "Hi Hello, thank you for calling Inspira Financial. What can I help you with today? I would also like to withdraw money from my account To help you with that, I'll need to verify your identity." --language English --speaker Aiden --instruct "Speak in very professional tone." --output excited.wav --play
+[connect] ws://localhost:8003/ws/tts
+[connection-latency] 551.37 ms
+[accepted] request_id=bfed4d67-1c9a-4dc6-b508-93b41b626799 speaker=Aiden language=English
+[audio-start] sample_rate=24000 audio_duration_s=12.56
+[first-audio] 25027.79 ms
+
+================================================================================
+CLIENT LATENCY
+================================================================================
+Connection latency : 551.37 ms
+Send call          : 0.41 ms
+CLIENT TTFB        : 266.24 ms
+CLIENT TTFT/TTFA   : 25027.79 ms
+Audio -> Done      : 812.78 ms
+CLIENT TOTAL       : 25840.57 ms
+
+================================================================================
+SERVER LATENCY
+================================================================================
+SERVER TTFB        : 0.1 ms
+SERVER TTFT/TTFA   : 24031.86 ms
+SERVER FIRST AUDIO : 24031.86 ms
+SERVER INFERENCE   : 24027.23 ms
+SERVER AUDIO SEND  : 28.72 ms
+SERVER TOTAL       : 24060.58 ms
+AUDIO DURATION     : 12.56 s
+RTF                : 1.913
