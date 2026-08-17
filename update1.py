b2fad342aad2b4a470ea5f14a07ae9dd261bf2e8
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-08-17 19:27:30 +0530

    Update update1.py

--- update1.py
+++ update1.py
@@ -66,3 +66,88 @@ python qwen_cloudrun_loadtest.py --url wss://qwen3-tts-150916788856.us-central1.
   python client.py --text "Hello, thank you for calling Inspira Financial. How can I help you today?" --language English --speaker Aiden --instruct "Speak in a professional customer service tone." --output warm.wav --play
 py -3.11 cold_start_client.py --mode openai --file "C:\Users\re_nikitav\Documents\test.wav" --openai-url "https://nemotron-3-5-150916788856.us-central1.run.app/v1/audio/transcriptions" --language en-US --runs 2
 py -3.11 cold_start_client.py --mode websocket --mic --language en-US --duration 10 --runs 2 --delay 2
+                                                                                                              
+                                                                                                              
+                                                                                                              
+                                                                                                              (base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav# python3 -m pip install -U huggingface_hub
+Looking in indexes: https://pypi.org/simple, https://pypi.ngc.nvidia.com
+Requirement already satisfied: huggingface_hub in /root/anaconda3/lib/python3.7/site-packages (0.16.4)
+WARNING: Retrying (Retry(total=4, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProxyError('Cannot connect to proxy.', RemoteDisconnected('Remote end closed connection without response'))': /huggingface-hub/
+WARNING: Retrying (Retry(total=3, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProxyError('Cannot connect to proxy.', RemoteDisconnected('Remote end closed connection without response'))': /huggingface-hub/
+WARNING: Retrying (Retry(total=2, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProxyError('Cannot connect to proxy.', RemoteDisconnected('Remote end closed connection without response'))': /huggingface-hub/
+WARNING: Retrying (Retry(total=1, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProxyError('Cannot connect to proxy.', RemoteDisconnected('Remote end closed connection without response'))': /huggingface-hub/
+WARNING: Retrying (Retry(total=0, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProxyError('Cannot connect to proxy.', RemoteDisconnected('Remote end closed connection without response'))': /huggingface-hub/
+Requirement already satisfied: filelock in /root/anaconda3/lib/python3.7/site-packages (from huggingface_hub) (3.0.12)
+Requirement already satisfied: fsspec in /root/anaconda3/lib/python3.7/site-packages (from huggingface_hub) (2023.1.0)
+Requirement already satisfied: requests in /root/anaconda3/lib/python3.7/site-packages (from huggingface_hub) (2.31.0)
+Requirement already satisfied: tqdm>=4.42.1 in /root/anaconda3/lib/python3.7/site-packages (from huggingface_hub) (4.67.0)
+Requirement already satisfied: pyyaml>=5.1 in /root/anaconda3/lib/python3.7/site-packages (from huggingface_hub) (5.3)
+Requirement already satisfied: typing-extensions>=3.7.4.3 in /root/anaconda3/lib/python3.7/site-packages (from huggingface_hub) (4.7.1)
+Requirement already satisfied: packaging>=20.9 in /root/anaconda3/lib/python3.7/site-packages (from huggingface_hub) (24.0)
+Requirement already satisfied: importlib-metadata in /root/anaconda3/lib/python3.7/site-packages (from huggingface_hub) (1.5.0)
+Requirement already satisfied: zipp>=0.5 in /root/anaconda3/lib/python3.7/site-packages (from importlib-metadata->huggingface_hub) (3.15.0)
+Requirement already satisfied: charset-normalizer<4,>=2 in /root/anaconda3/lib/python3.7/site-packages (from requests->huggingface_hub) (3.4.0)
+Requirement already satisfied: idna<4,>=2.5 in /root/anaconda3/lib/python3.7/site-packages (from requests->huggingface_hub) (2.8)
+Requirement already satisfied: urllib3<3,>=1.21.1 in /root/anaconda3/lib/python3.7/site-packages (from requests->huggingface_hub) (2.0.7)
+Requirement already satisfied: certifi>=2017.4.17 in /root/anaconda3/lib/python3.7/site-packages (from requests->huggingface_hub) (2024.8.30)
+DEPRECATION: pyodbc 4.0.0-unsupported has a non-standard version number. pip 24.1 will enforce this behaviour change. A possible replacement is to upgrade to a newer version of pyodbc or contact the author to suggest that they release a version with a conforming version number. Discussion can be found at https://github.com/pypa/pip/issues/12063
+WARNING: Running pip as the 'root' user can result in broken permissions and conflicting behaviour with the system package manager. It is recommended to use a virtual environment instead: https://pip.pypa.io/warnings/venv
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav# hf --version
+hf: command not found
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav# unset http_proxy
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav# unset https_proxy
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav# hf --version
+hf: command not found
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav# python3 -m pip install -U huggingface_hub
+Looking in indexes: https://pypi.org/simple, https://pypi.ngc.nvidia.com
+Requirement already satisfied: huggingface_hub in /root/anaconda3/lib/python3.7/site-packages (0.16.4)
+WARNING: Retrying (Retry(total=4, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProtocolError('Connection aborted.', ConnectionResetError(104, 'Connection reset by peer'))': /simple/huggingface-hub/
+WARNING: Retrying (Retry(total=3, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProtocolError('Connection aborted.', ConnectionResetError(104, 'Connection reset by peer'))': /simple/huggingface-hub/
+WARNING: Retrying (Retry(total=2, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProtocolError('Connection aborted.', ConnectionResetError(104, 'Connection reset by peer'))': /simple/huggingface-hub/
+WARNING: Retrying (Retry(total=1, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProtocolError('Connection aborted.', ConnectionResetError(104, 'Connection reset by peer'))': /simple/huggingface-hub/
+WARNING: Retrying (Retry(total=0, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProtocolError('Connection aborted.', ConnectionResetError(104, 'Connection reset by peer'))': /simple/huggingface-hub/
+WARNING: Retrying (Retry(total=4, connect=None, read=None, redirect=None, status=None)) after connection broken by 'NewConnectionError('<pip._vendor.urllib3.connection.HTTPSConnection object at 0x7d44a7dea0d0>: Failed to establish a new connection: [Errno -2] Name or service not known')': /huggingface-hub/
+WARNING: Retrying (Retry(total=3, connect=None, read=None, redirect=None, status=None)) after connection broken by 'NewConnectionError('<pip._vendor.urllib3.connection.HTTPSConnection object at 0x7d44a7dea310>: Failed to establish a new connection: [Errno -2] Name or service not known')': /huggingface-hub/
+WARNING: Retrying (Retry(total=2, connect=None, read=None, redirect=None, status=None)) after connection broken by 'NewConnectionError('<pip._vendor.urllib3.connection.HTTPSConnection object at 0x7d44a7de0a90>: Failed to establish a new connection: [Errno -2] Name or service not known')': /huggingface-hub/
+WARNING: Retrying (Retry(total=1, connect=None, read=None, redirect=None, status=None)) after connection broken by 'NewConnectionError('<pip._vendor.urllib3.connection.HTTPSConnection object at 0x7d44a7de0890>: Failed to establish a new connection: [Errno -2] Name or service not known')': /huggingface-hub/
+WARNING: Retrying (Retry(total=0, connect=None, read=None, redirect=None, status=None)) after connection broken by 'NewConnectionError('<pip._vendor.urllib3.connection.HTTPSConnection object at 0x7d44a7de0510>: Failed to establish a new connection: [Errno -2] Name or service not known')': /huggingface-hub/
+Requirement already satisfied: filelock in /root/anaconda3/lib/python3.7/site-packages (from huggingface_hub) (3.0.12)
+Requirement already satisfied: fsspec in /root/anaconda3/lib/python3.7/site-packages (from huggingface_hub) (2023.1.0)
+Requirement already satisfied: requests in /root/anaconda3/lib/python3.7/site-packages (from huggingface_hub) (2.31.0)
+Requirement already satisfied: tqdm>=4.42.1 in /root/anaconda3/lib/python3.7/site-packages (from huggingface_hub) (4.67.0)
+Requirement already satisfied: pyyaml>=5.1 in /root/anaconda3/lib/python3.7/site-packages (from huggingface_hub) (5.3)
+Requirement already satisfied: typing-extensions>=3.7.4.3 in /root/anaconda3/lib/python3.7/site-packages (from huggingface_hub) (4.7.1)
+Requirement already satisfied: packaging>=20.9 in /root/anaconda3/lib/python3.7/site-packages (from huggingface_hub) (24.0)
+Requirement already satisfied: importlib-metadata in /root/anaconda3/lib/python3.7/site-packages (from huggingface_hub) (1.5.0)
+Requirement already satisfied: zipp>=0.5 in /root/anaconda3/lib/python3.7/site-packages (from importlib-metadata->huggingface_hub) (3.15.0)
+Requirement already satisfied: charset-normalizer<4,>=2 in /root/anaconda3/lib/python3.7/site-packages (from requests->huggingface_hub) (3.4.0)
+Requirement already satisfied: idna<4,>=2.5 in /root/anaconda3/lib/python3.7/site-packages (from requests->huggingface_hub) (2.8)
+Requirement already satisfied: urllib3<3,>=1.21.1 in /root/anaconda3/lib/python3.7/site-packages (from requests->huggingface_hub) (2.0.7)
+Requirement already satisfied: certifi>=2017.4.17 in /root/anaconda3/lib/python3.7/site-packages (from requests->huggingface_hub) (2024.8.30)
+DEPRECATION: pyodbc 4.0.0-unsupported has a non-standard version number. pip 24.1 will enforce this behaviour change. A possible replacement is to upgrade to a newer version of pyodbc or contact the author to suggest that they release a version with a conforming version number. Discussion can be found at https://github.com/pypa/pip/issues/12063
+WARNING: Running pip as the 'root' user can result in broken permissions and conflicting behaviour with the system package manager. It is recommended to use a virtual environment instead: https://pip.pypa.io/warnings/venv
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav# ls
+Fish_audio_s1mini  fish_speech_1.5  nemotron_finetuned  nemotron_finetuned_fixed  nemotron_finetuned_updated  quen_3_tts
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav# cd quen_3_tts/
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/quen_3_tts# vi Dockerfile
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/quen_3_tts# export http_proxy=http_proxy="http://163.116.128.80:8080
+> ^C
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/quen_3_tts# export http_proxy=http://163.116.128.80:8080
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/quen_3_tts# export https_proxy=http://163.116.128.80:8080
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/quen_3_tts# python3 -m pip install -U huggingface_hub
+Looking in indexes: https://pypi.org/simple, https://pypi.ngc.nvidia.com
+Requirement already satisfied: huggingface_hub in /root/anaconda3/lib/python3.7/site-packages (0.16.4)
+WARNING: Retrying (Retry(total=4, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProxyError('Cannot connect to proxy.', RemoteDisconnected('Remote end closed connection without response'))': /huggingface-hub/
+WARNING: Retrying (Retry(total=3, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProxyError('Cannot connect to proxy.', RemoteDisconnected('Remote end closed connection without response'))': /huggingface-hub/
+WARNING: Retrying (Retry(total=2, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProxyError('Cannot connect to proxy.', RemoteDisconnected('Remote end closed connection without response'))': /huggingface-hub/
+^CERROR: Operation cancelled by user
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/quen_3_tts# export https_proxy="http://163.116.128.80:8080"
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/quen_3_tts# export http_proxy="http://163.116.128.80:8080"
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/quen_3_tts# python3 -m pip install -U huggingface_hub
+Looking in indexes: https://pypi.org/simple, https://pypi.ngc.nvidia.com
+Requirement already satisfied: huggingface_hub in /root/anaconda3/lib/python3.7/site-packages (0.16.4)
+WARNING: Retrying (Retry(total=4, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProxyError('Cannot connect to proxy.', RemoteDisconnected('Remote end closed connection without response'))': /huggingface-hub/
+WARNING: Retrying (Retry(total=3, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProxyError('Cannot connect to proxy.', RemoteDisconnected('Remote end closed connection without response'))': /huggingface-hub/
+WARNING: Retrying (Retry(total=2, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProxyError('Cannot connect to proxy.', RemoteDisconnected('Remote end closed connection without response'))': /huggingface-hub/
+WARNING: Retrying (Retry(total=1, connect=None, read=None, redirect=None, status=None)) after connection broken by 'ProxyError('Cannot connect to proxy.', RemoteDisconnected('Remote end closed connection without response'))': /huggingface-hub/
+^CERROR: Operation cancelled by user
